9fb55a74b8a1df77c59ce4b978aa0a5e145ed2e2
David69699 <abutidzedato18@gmail.com>
2024-12-11 18:43:31 +0400

    noice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,156 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import Map from "../../public/images/map.svg";
+import Globe from "../../public/images/globe.svg";
+import Ground from "../../public/images/ground.svg";
+import Entire from "../../public/images/headermap.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col justify-center items-center p-8">
+      <section>
+        <div>
+          <Image className="relative" src={Entire} alt="" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </section>
+      <div className="flex gap-8 justify-center items-center">
+        <h1 className="text-black font-poppins text-4xl font-normal leading-normal">
+          Our Impact Worldwide
+        </h1>
+        <div className="h-[1px] w-[888px] bg-green-600"></div>
+      </div>
+
+      <div>
+        <Image src={Map} alt="Map" className="bg-blend-darken" />
+      </div>
+
+      <div className="flex gap-8 justify-center items-center">
+        <h1 className="text-black font-poppins text-4xl font-normal leading-normal">
+          Key Impact Areas
+        </h1>
+        <div className="h-[1px] w-[888px] bg-green-600"></div>
+      </div>
+
+      <section>
+        <motion.div
+          initial={{ opacity: 0, x: -1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Image src={Globe} alt="Globe" className="absolute left-[-70px]" />
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal mb-5">
+              Comprehensive Regulatory Compliance
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Proactively cover international, national, and state-wide
+              regulations and protocols in real-time, ensuring alignment with
+              the latest standards and fostering global, national, and local
+              regulatory adherence.
+            </p>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal text-right mb-5">
+              Carbon Footprint Reduction
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Implement strategies to lower carbon emissions across all
+              production processes, prioritizing low-impact practices,
+              sustainable energy use, and emission-reducing technologies to
+              support a cleaner environment.
+            </p>
+          </div>
+          <Image src={Ground} alt="Globe" className="absolute right-[-70px]" />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: -1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Image src={Globe} alt="Globe" className="absolute left-[-70px]" />
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal mb-5">
+              Cost Efficiency through Sustainability:
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Drive cost savings by optimizing resource usage, reducing waste,
+              and adopting sustainable practices that streamline production,
+              leading to long-term financial benefits.
+            </p>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
+        >
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal text-right mb-5">
+              Innovation-Driven Sustainability:
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Foster a culture of continuous improvement and innovation,
+              adopting cutting-edge sustainable technologies and practices that
+              set a new industry standard for environmentally responsible
+              production.
+            </p>
+          </div>
+          <Image src={Ground} alt="Globe" className="absolute right-[-70px]" />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: -1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
+        >
+          <Image src={Globe} alt="Globe" className="absolute left-[-70px]" />
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal mb-5">
+              Positive Environmental Stewardship:
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Actively demonstrate environmental responsibility, enhancing the
+              organization’s reputation as a leader in sustainability and
+              contributing positively to the communities and ecosystems involved
+              in production.
+            </p>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 1200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          className="bg-green-200 flex items-start flex-col justify-center greendiv mt-[77px] relative"
+        >
+          <div className="w-[781px]">
+            <h1 className="text-[#0C4E0A] font-poppins text-2xl font-normal leading-normal text-right mb-5">
+              Enhanced Long-Term Brand Value:
+            </h1>
+            <p className="text-black font-poppins text-base font-normal leading-normal">
+              Build a resilient and trusted brand by prioritizing sustainable
+              practices that resonate with consumers, partners, and
+              stakeholders, establishing long-term value and credibility.
+            </p>
+          </div>
+          <Image src={Ground} alt="Globe" className="absolute right-[-70px]" />
+        </motion.div>
+      </section>
     </div>
   );
 }
