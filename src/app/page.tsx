30658430a16c693f85016bb6be04c62804f0ef4b
David69699 <abutidzedato18@gmail.com>
2024-12-16 19:30:19 +0400

    feat(): added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,90 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import Map from "../../public/images/map.svg";
 import Globe from "../../public/images/globe.svg";
 import Ground from "../../public/images/ground.svg";
-import Entire from "../../public/images/headermap.svg";
+import Entire from "../../public/images/entiremap.svg";
+import Cloud from "../../public/images/cloud.png";
+import Success from "../../public/images/success.svg";
+import { useState } from "react";
 
 export default function Home() {
+  const CO2_VALUES = [10000, 12000, 15000, 20000, 25000, 30000, 35000];
+  const SATISFACTION_VALUES = [80, 85, 88, 90, 95, 98, 100];
+
+  const [co2Value, setCO2Value] = useState(CO2_VALUES[0]);
+  const [satisfactionValue, setSatisfactionValue] = useState(
+    SATISFACTION_VALUES[0]
+  );
+
+  let co2Interval: NodeJS.Timeout;
+  let satisfactionInterval: NodeJS.Timeout;
+
+  const handleHover = () => {
+    let co2Index = 0;
+    let satisfactionIndex = 0;
+
+    co2Interval = setInterval(() => {
+      setCO2Value(CO2_VALUES[co2Index]);
+      co2Index++;
+      if (co2Index === CO2_VALUES.length) clearInterval(co2Interval);
+    }, 200);
+
+    satisfactionInterval = setInterval(() => {
+      setSatisfactionValue(SATISFACTION_VALUES[satisfactionIndex]);
+      satisfactionIndex++;
+      if (satisfactionIndex === SATISFACTION_VALUES.length)
+        clearInterval(satisfactionInterval);
+    }, 200);
+  };
+
+  const handleLeave = () => {
+    clearInterval(co2Interval);
+    clearInterval(satisfactionInterval);
+    setCO2Value(CO2_VALUES[0]);
+    setSatisfactionValue(SATISFACTION_VALUES[0]);
+  };
   return (
-    <div className="flex flex-col justify-center items-center p-8">
-      <section>
-        <div>
-          <Image className="relative" src={Entire} alt="" />
+    <div className="flex flex-col justify-center items-center h-full">
+      <section className="relative">
+        <div
+          className="relative cursor-pointer"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          <Image
+            className="w-full h-auto opacity-70"
+            src={Entire}
+            alt="Background Image"
+          />
+
+          <div className="absolute top-0 w-full h-full flex flex-col items-center justify-center text-center text-green-700">
+            <div className="flex justify-around w-3/4 mx-auto">
+              <div className="flex flex-col items-center">
+                <Image className="mb-2" src={Cloud} alt="CO2 Icon" />
+                <p className="text-[#023300] font-poppins text-2xl font-normal leading-normal">
+                  Tons Of CO2 Reduced
+                </p>
+                <p className="text-[#023300] font-poppins text-4xl font-semibold leading-normal">
+                  {co2Value.toLocaleString()} +
+                </p>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <Image className="mb-2" src={Success} alt="Success Icon" />
+                <p className="text-[#023300] font-poppins text-2xl font-normal leading-normal">
+                  Customer Satisfaction
+                </p>
+                <p className="text-[#023300] font-poppins text-4xl font-semibold leading-normal">
+                  {satisfactionValue} %
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+
       <div className="flex gap-8 justify-center items-center">
         <h1 className="text-black font-poppins text-4xl font-normal leading-normal">
           Our Impact Worldwide
@@ -151,6 +222,8 @@ export default function Home() {
           <Image src={Ground} alt="Globe" className="absolute right-[-70px]" />
         </motion.div>
       </section>
+
+      <footer className="w-screen h-[464px] bg-green-700 mt-[140px]"></footer>
     </div>
   );
 }
